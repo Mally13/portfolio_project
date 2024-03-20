243b812ae9c9df835d5bb00cf437ffb11a42a809
Mally13 <mallymutuku13@gmail.com>
2024-03-20 23:41:11 +0300

    Feat: Homepage

--- src/components/LandingHeader.js
+++ src/components/LandingHeader.js
@@ -14,6 +14,12 @@ export const LandingHeader = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
+                  href="/home"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Get Started
+                </a>{" "}
+                <a
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
                 >
